325d7b631d2f505909c47314fdf264587f228969
MDrakos <mike.drakos@gmail.com>
2017-07-26 09:25:27 -0700

    Initial commit

---
+++ MAC Interface Demo/Debug/src/ASF/thirdparty/fatfs/fatfs-r0.09/src/ff.d
@@ -0,0 +1,21 @@
+src/asf/thirdparty/fatfs/fatfs-r0.09/src/ff.d \
+ src/asf/thirdparty/fatfs/fatfs-r0.09/src/ff.o: \
+ ../src/asf/thirdparty/fatfs/fatfs-r0.09/src/ff.c \
+ ../src/asf/thirdparty/fatfs/fatfs-r0.09/src/ff.h \
+ ../src/asf/thirdparty/fatfs/fatfs-r0.09/src/integer.h \
+ ../src/asf/thirdparty/fatfs/fatfs-r0.09/src/ffconf.h \
+ ../src/config/conf_fatfs.h \
+ ../src/asf/thirdparty/fatfs/fatfs-r0.09/src/diskio.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\lib\gcc\arm-none-eabi\5.3.1\include\stdarg.h
+
+../src/asf/thirdparty/fatfs/fatfs-r0.09/src/ff.h:
+
+../src/asf/thirdparty/fatfs/fatfs-r0.09/src/integer.h:
+
+../src/asf/thirdparty/fatfs/fatfs-r0.09/src/ffconf.h:
+
+../src/config/conf_fatfs.h:
+
+../src/asf/thirdparty/fatfs/fatfs-r0.09/src/diskio.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\lib\gcc\arm-none-eabi\5.3.1\include\stdarg.h:
---
+++ MAC Interface Demo/src/main.c
@@ -0,0 +1,169 @@
+/*
+* Copyright 2015 Rafael Roman Otero.
+*
+* This file is part of Xbee MAC Interface.
+*
+* Xbee MAC Interface is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+/**
+* RSSI example
+*
+* Node blinks the boards LED, and sends a message every now and then. Blinking rate
+* depends on the RSSI of the last message
+*/
+
+#include <asf.h>
+#include "mac/mac.h"
+#include "radio/radio.h"
+#include "message.h"
+
+#define ADDRESSEE_NODE		5 //Your address
+#define LED_ON				false
+#define LED1 IOPORT_CREATE_PIN(PIOC, 20);
+#define LED2 IOPORT_CREATE_PIN(PIOA, 16);
+#define LED3 IOPORT_CREATE_PIN(PIOC, 22);
+
+
+void msg_received(Message *msg);
+void ack_received(uint8_t status);
+void task_msgsend(bool msg_end);
+void task_ledblink(void);
+void oled_update(void);
+void oled_pause(void);
+
+static uint32_t blink_wait = 2000; //2 secs initial wait
+uint8_t msg_state = 1;
+bool button0_state = false;
+uint32_t value[6];
+
+void Button_Handler(uint32_t id, uint32_t mask)
+{
+	uint32_t led;
+	if( ID_PIOA == id && PIO_PA2 == mask )
+	{ 
+		button0_state = !button0_state; 
+	}
+	else { return; }
+	ioport_set_pin_level( led, !ioport_get_pin_level(led) );
+}
+
+
+const uint32_t irq_priority = 5;
+void configure_buttons(void)
+{
+	//Configure Pushbutton 0
+	pmc_enable_periph_clk(ID_PIOA);
+	pio_set_debounce_filter(PIOC, PIN_PUSHBUTTON_0_MASK, 10);
+	pio_handler_set(PIOA, ID_PIOA,
+	PIN_PUSHBUTTON_0_MASK, PIN_PUSHBUTTON_0_ATTR, Button_Handler);
+	NVIC_EnableIRQ((IRQn_Type) ID_PIOA);
+	pio_handler_set_priority(PIOA, (IRQn_Type) ID_PIOA, irq_priority);
+	pio_enable_interrupt(PIOA, PIN_PUSHBUTTON_0_MASK);
+}
+
+void main(void)
+{
+	sysclk_init();
+	board_init();
+	configure_buttons();
+	ssd1306_init();
+	
+	//Init MAC
+	if( !mac_init( msg_received, ack_received ) )
+	{
+		//Error: Xbee's baud rate is different to the one specified in mac_config.h (RADIO SPEED RATE)
+		while(1);
+	}
+	
+	//signal initialization went fine.
+	ioport_set_pin_level( IOPORT_CREATE_PIN(PIOC, 23), LED_ON );
+	while (1)
+	{
+		while(button0_state == false)
+		{
+			delay_ms(1000);
+			ioport_set_pin_level( IOPORT_CREATE_PIN(PIOC, 23), !LED_ON );
+			oled_pause();
+			task_msgsend(false);
+		}
+	
+		while (msg_state == 1) 
+		{
+			if (ADDRESSEE_NODE == 1)
+				break;
+			oled_pause();
+			task_msgsend(false);
+		}
+	
+		while(button0_state == true && msg_state == 0)
+		{
+			oled_update();
+			task_msgsend(true);
+			task_ledblink();
+		
+			delay_ms(blink_wait);
+		}
+	}
+}
+
+void task_msgsend(bool msg_end){
+	//sends a message every now and then
+	Message msg;
+	msg.address = ADDRESSEE_NODE;			//Addressee node
+	//msg.data[0] = 7;						//send anything (random value)
+	msg.data_length = 1;					//we're sending one byte
+	if (msg_end == false)
+	{
+		msg.data[0] = 1;
+	}
+	else
+	{
+		msg.data[0] = 0;
+	}
+	mac_send(&msg);
+}
+
+static bool led_state = LED_ON;
+
+void task_ledblink(void){
+	//blinks led. Blink rate depends on rssi received
+	ioport_set_pin_level( IOPORT_CREATE_PIN(PIOC, 23), led_state = !led_state );
+}
+
+
+void msg_received(Message *msg_in){
+	//updates the blink wait time, based on the RSSI of msg received
+	blink_wait = msg_in->rssi * 18 - 700;
+	msg_state = msg_in->data[0];
+}
+
+void ack_received(uint8_t status){}
+	
+void oled_update()
+{
+	ssd1306_clear();
+	ssd1306_set_page_address(0);
+	sprintf(value, "%d", blink_wait);
+	ssd1306_write_text("Blink wait (ms): ");
+	ssd1306_write_text(value);
+}
+
+void oled_pause()
+{
+	ssd1306_clear();
+	ssd1306_set_page_address(0);
+	ssd1306_write_text("Not receiving");
+}
